9e56221fd470f84ba460be7354a6f1f321ae34a5
Carles Climent <carlescliment@hotmail.com>
2013-06-17 12:05:50 +0200

    List all posts

--- src/SfVlc/MainBundle/Tests/Controller/AdminControllerTest.php
+++ src/SfVlc/MainBundle/Tests/Controller/AdminControllerTest.php
@@ -6,13 +6,14 @@ class AdminControllerTest extends BaseControllerTest {
 
     public function setUp() {
         parent::setUp();
-        $this->truncateTables(array('users'));
+        $this->truncateTables(array('users', 'news'));
     }
 
     /**
      * @test
      */
-    public function IShouldNotAccessToTheAdminInterfaceIfNotAdmin() {
+    public function IShouldNotAccessToTheAdminInterfaceIfNotAdmin()
+    {
         // Arrange
         $not_admin = $this->createUser('not_admin', 'password');
         $this->login('not_admin', 'password');
@@ -27,7 +28,8 @@ class AdminControllerTest extends BaseControllerTest {
     /**
      * @test
      */
-    public function IShouldAccessToTheAdminInterfaceIfAdmin() {
+    public function IShouldAccessToTheAdminInterfaceIfAdmin()
+    {
         // Arrange
         $admin = $this->createUser('admin', 'password', 'ROLE_ADMIN');
         $this->login('admin', 'password');
@@ -37,5 +39,27 @@ class AdminControllerTest extends BaseControllerTest {
 
         // Assert
         $this->assertTrue($this->client->getResponse()->isSuccessful());
+    }
+
+
+    /**
+     * @test
+     */
+    public function IShouldAccessToTheListOfPosts()
+    {
+        // Arrange
+        $admin = $this->createUser('admin', 'password', 'ROLE_ADMIN');
+        $this->login('admin', 'password');
+        $post_manager = $this->getService('blade_tester_light_news.news_manager');
+        $post_manager->create('One title');
+        $post_manager->create('Other title');
+        $this->em->flush();
+
+        // Act
+        $crawler = $this->visit('sf_vlc_admin_posts');
+
+        // Assert
+        $posts_shown = $crawler->filter('.post')->count();
+        $this->assertEquals(2, $posts_shown);
     }
 }
