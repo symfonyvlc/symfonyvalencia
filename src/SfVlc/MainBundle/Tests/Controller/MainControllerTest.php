7b6c71fb28ae8ba8bcaae5dbd40042c6d451f4df
Carles Climent <carlescliment@hotmail.com>
2013-06-17 11:26:28 +0200

    Admin interface: Block access to non-admins

--- src/SfVlc/MainBundle/Tests/Controller/MainControllerTest.php
+++ src/SfVlc/MainBundle/Tests/Controller/MainControllerTest.php
@@ -2,10 +2,7 @@
 
 namespace SfVlc\MainBundle\Tests\Controller;
 
-use BladeTester\HandyTestsBundle\Model\HandyTestCase;
-
-
-class MainControllerTest extends HandyTestCase {
+class MainControllerTest extends BaseControllerTest {
 
 
     public function setUp() {
@@ -83,26 +80,6 @@ class MainControllerTest extends HandyTestCase {
 
         // Assert
         $this->assertTrue($crawler->filter('a#intranet-link')->count() > 0);
-    }
-
-    private function createUser($username, $password) {
-        $userManager = $this->client->getKernel()->getContainer()->get('fos_user.user_manager');
-        $user = $userManager->createUser();
-        $user->setUserName($username);
-        $user->setEmail($username . '@test.is');
-        $user->setPlainPassword($password);
-        $user->setEnabled(true);
-        $userManager->updateUser($user);
-        $this->em->flush();
-        return $user;
-    }
-
-    private function login($username, $password) {
-        $crawler = $this->visit('fos_user_security_login');
-        $form = $crawler->filter('form#login')->form();
-        $form['_username'] = $username;
-        $form['_password'] = $password;
-        $this->client->submit($form);
     }
 
 }
