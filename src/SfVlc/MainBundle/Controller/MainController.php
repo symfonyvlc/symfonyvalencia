89a572f53fb625e0d0efe14b44b6d9b236a07b7e
Carles Climent <carlescliment@hotmail.com>
2013-06-17 12:55:00 +0200

    Sorting posts by date

--- src/SfVlc/MainBundle/Controller/MainController.php
+++ src/SfVlc/MainBundle/Controller/MainController.php
@@ -14,7 +14,7 @@ class MainController extends Controller
     public function indexAction()
     {
         $post_manager = $this->get('blade_tester_light_news.news_manager');
-        $posts = $post_manager->findAll(array(), array('createdAt' => 'DESC'));
+        $posts = $post_manager->findBy(array(), array('createdAt' => 'DESC'));
         return array(
             'posts' => $posts
             );
